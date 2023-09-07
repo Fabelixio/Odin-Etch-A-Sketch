a4ec9c61f1f887ade17e44fb5e1aa8e9511ef688
Fabelixio <felixsewellknight@gmail.com>
2023-09-07 13:10:33 +0100

    add function to create grid

--- javaScript.js
+++ javaScript.js
@@ -1,16 +1,17 @@
 /*Being by creating the grid. Create the squares using CSS,
 put the grid squares inside the container div. Create the grid
 with JS by creating a 16x16 div grid*/
-let columns = 16
-let rows = 16
-    for(let i = 0; i < columns; i++) {
-        let column = document.createElement('div')
-        column.className = 'column'
-        for(let j = 0; j < rows; j++) {
-            let row = document.createElement('div')
-            row.className = 'row'
-            row.textContent = i + '-' + j
-            column.appendChild(row)
-        }
-        gridContainer.appendChild(column)
+
+const container = document.getElementById('container')
+//make grid function
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows)
+    container.style.setProperty('--grid-cols', cols)
+    for(c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div")
+        cell.innerText = (c + 1)
+        container.appendChild(cell).className = 'grid-item'
     }
+}
+makeRows(16, 16)
+
