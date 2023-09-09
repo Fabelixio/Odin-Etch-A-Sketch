695679a029c53cccf087836180768e358a2e6da4
Fabelixio <felixsewellknight@gmail.com>
2023-09-09 15:27:18 +0100

    add button functionality

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,4 @@
+//Create the grid with size based on number input, default to 16x16
 function makeGrid(number) {
     const gridContainer = document.querySelector('.container')
     gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`
@@ -9,7 +10,7 @@ function makeGrid(number) {
     }
     document.querySelector('.grid-dimensions').textContent = `${number}x${number}`
 }
-
+//Make grid boxes change color on mouse over
 let gridNumber = 16
 makeGrid(gridNumber)
 let gridBoxList = document.querySelectorAll(".grid-box")
@@ -20,6 +21,35 @@ gridBoxList.forEach(gridbox => {
 function changeColor(event) {
     event.target.style.backgroundColor = 'black'
 }
+//Clear the previous grid and make a new one when user presses button based on prompt
+function resetGrid() {
+    let userNum = prompt("select a grid size less than 100:")
+    if(userNum === null || userNum === undefined || userNum === "") {
+        return
+    } 
+    while(userNum > 100) {
+        userNum = prompt("Select a smaller number")
+    }
+    const gridContainer = document.querySelector('.container')
+    while(gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+    makeGrid(userNum)
+    let gridBoxList = document.querySelectorAll('.grid-box')
+    gridBoxList.forEach((gridBox) => {
+        gridBox.addEventListener('mouseover', changeColor)
+    })
+}
+//When pressed return the colours back to default
+function buttonClear() {
+    let gridBoxList = document.querySelectorAll('.grid-box')
+    gridBoxList.forEach((gridBox) => gridBox.style.backgroundColor = null)
+}
+const clearBTN = document.querySelector('.clear-button')
+clearBTN.addEventListener('click', buttonClear)
 
-//Add button functionality to reset grid and resize.
+const resetBTN = document.querySelector('.reset-button')
+resetBTN.addEventListener('click', resetGrid)
+
+//Add button functionality to resize.
 //Maybe add ability to change to multicolour.
